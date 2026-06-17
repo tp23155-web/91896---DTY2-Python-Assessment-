a9cc8eb266a2542e211daa4a0e0db856418dfbba
tp23155-web <tp23155@mbc.school.nz>
2026-06-18 11:14:21 +1200

    18/6/26

---
+++ Instructions.py
@@ -0,0 +1,18 @@
+def instructions():
+    ('''
+     For each set of items hired by a single customer, enter...
+
+     - Customer full name
+
+     - specific item 
+
+     - number of items hired 
+
+     When an item is returned, the order will be added to a seperate returned section:
+
+     There will be an option to exit the program early, allowing the user to quit if they did not wish to continue.
+
+     All data will be stored on a seperate .txt file for reviewing later.
+     
+     ''')
+instructions()
