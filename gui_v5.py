23e17aa1cd4e43b274296547e5b37cd073fe112c
tp23155-web <tp23155@mbc.school.nz>
2026-06-29 14:26:24 +1200

    29/6

--- gui_v5.py
+++ gui_v5.py
@@ -7,6 +7,25 @@ item_hired = []
 quantity_hired = []
 item_receipts = []
 
+def instructions():
+    ('''
+     For each set of items hired by a single customer, enter...
+
+     - Customer full name
+
+     - specific item 
+
+     - number of items hired 
+
+     When an item is returned, the order will be added to a seperate returned section:
+
+     There will be an option to exit the program early, allowing the user to quit if they did not wish to continue.
+
+     All data will be stored on a seperate .txt file for reviewing later.
+     
+     ''')
+instructions()
+
 def save_data():
     with open("party_hire.txt", "w") as file:
         file.write("----------------------------------------------------------------------------------------------------\n")
